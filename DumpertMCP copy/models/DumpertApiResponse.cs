089f3d88f9b29127e2a0e1b6476d2ffa2cebdc46
Rutger Roffel <rroffel@newnexus.nl>
2025-06-11 21:33:30 +0200

    Remove duplicate files
    lol

--- DumpertMCP copy/models/DumpertApiResponse.cs
+++
@@ -1,9 +0,0 @@
-using System.Text.Json.Serialization;
-
-namespace DumpertMCP.models;
-
-public class DumpertApiResponse
-{
-    [JsonPropertyName("items")]
-    public List<DumpertItem>? Items { get; set; }
-}
